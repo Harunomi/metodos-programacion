35a15ef3539b71a5a6768f1aafd21326408df9ec
Cristhofer Parada <cristhofer.parada@usach.cl>
2021-04-29 15:53:15 -0400

    Add files via upload

---
+++ exp1.c
@@ -0,0 +1,37 @@
+#include <stdio.h> // Biblioteca estandar de C 
+#include <stdlib.h> // Bilbioteca para convertir numeros a caracteres en ASCII
+int main(){
+        /* El siguiente programa imprime por pantalla un ASCII art qe dibuje una bomba */
+        printf("                 .\n");
+        printf("                 .\n");
+        printf("                 .       :\n");
+        printf("                 :      .\n");
+        printf("        :..   :  : :  .\n");
+        printf("           ..  ; :: .\n");
+        printf("              ... .. :..\n");
+        printf("             ::: :...\n");
+        printf("         ::.:.:...;; .....\n");
+        printf("      :..     .;.. :;     ..\n");
+        printf("            . :. .  ;.\n");
+        printf("             .: ;;: ;.\n");
+        printf("            :; .BRRRV;\n");
+        printf("               YB BMMMBR\n");
+        printf("              ;BVIMMMMMt\n");
+        printf("        .=YRBBBMMMMMMMB\n");
+        printf("      =RMMMMMMMMMMMMMM;\n");
+        printf("    ;BMMR=VMMMMMMMMMMMV.\n");
+        printf("   tMMR::VMMMMMMMMMMMMMB:\n");
+        printf("  tMMt ;BMMMMMMMMMMMMMMMB.\n");
+        printf(" ;MMY ;MMMMMMMMMMMMMMMMMMV\n");
+        printf(" XMB .BMMMMMMMMMMMMMMMMMMM:\n");
+        printf(" BMI +MMMMMMMMMMMMMMMMMMMMi\n");
+        printf(".MM= XMMMMMMMMMMMMMMMMMMMMY\n");
+        printf(" BMt YMMMMMMMMMMMMMMMMMMMMi\n");
+        printf(" VMB +MMMMMMMMMMMMMMMMMMMM:\n");
+        printf(" ;MM+ BMMMMMMMMMMMMMMMMMMR\n");
+        printf("  tMBVBMMMMMMMMMMMMMMMMMB.\n");
+        printf("   tMMMMMMMMMMMMMMMMMMMB:\n");
+        printf("    ;BMMMMMMMMMMMMMMMMY\n");
+        printf("      +BMMMMMMMMMMMBY:\n");
+        printf("        :+YRBBBRVt;\n");
+        }
